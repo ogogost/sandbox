32b59be80ac9ff4f397271ce7bdb67242d29fa68
Galsan Dampilon <59030438+ogogost@users.noreply.github.com>
2020-01-29 17:14:08 +0300

    Update test.py
    игра на кубик

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-data = ['a10', 'a8', 'aj', 'aq']
-count = data
+# data = []
+import random
+import time
+win = 0
+print("Welcome to our best casino in the world!")
+print("You must try to guess kubik number, it is random factor")
 
-# def new(arg):
-for i in range(len(data)):
-    count[i] = data[i][1:3]
+while win == 0:
 
-# new(data)
-print(count)
+    player1_input = int(input("Player1 input number:"))
+    player2_input = int(input("Player2 input number:"))
+    kubik = random.randint(1,6)
+    print('3')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+    print("Kubik number is:", kubik)
+
+    if player1_input == kubik:
+        print("Player 1 win!")
+        win = 1
+    elif player2_input == kubik:
+        print("Player 2 win!")
+        win = 1
+    else:
+        print("Nobody win, play again")
